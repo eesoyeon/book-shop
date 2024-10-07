16ac7e54d54bcf0520de723db84b189d0edae377
Soyeon Lee <pink731p@gmail.com>
2024-10-07 16:17:03 +0900

    좋아요 추가&삭제 구현

--- routes/likes.js
+++ routes/likes.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
-const { body, param, validationResult } = require('express-validator');
+// const { body, param, validationResult } = require('express-validator');
+const { addLike, removeLike } = require('../controller/LikeController');
 
 router.use(express.json());
 
 // 좋아요 추가
-router.post('/:id', (req, res) => {
-    res.json('좋아요 추가');
-});
+router.post('/:id', addLike);
 
 // 좋아요 삭제
-router.delete('/:id', (req, res) => {
-    res.json('좋아요 삭제');
-});
+router.delete('/:id', removeLike);
 
 module.exports = router;
